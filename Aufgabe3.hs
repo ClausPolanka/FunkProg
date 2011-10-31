c37c94677204aedc97ab7723a227401e49c17448
Claus Polanka <claus.polanka@chello.at>
2011-10-31 12:48:44 +0100

    Renamed some methods to better reveal intend.

--- Aufgabe3.hs
+++ Aufgabe3.hs
@@ -16,16 +16,17 @@ anp2 :: [[Integer]] -> Zeilen -> Spalten -> Fuellwert -> Matrix
 anp2 _ 0 _ _ = []
 anp2 _ _ 0 _ = []
 anp2 l z s w 
-    | l == [] = replicate (fromIntegral z) (replicate (fromIntegral s) w)
-    | length l < (fromInteger z) = extend_L
-    | otherwise = crop_L
+    | l == [] = replicate (fromIntegral z) requiredComponentList
+    | length l < (fromInteger z) = extended_L
+    | otherwise = cropped_L
         where
-            extend_L = customizedComponentLists ++ requiredListsFilledWithWord
-            crop_L = take (fromInteger z) customizedComponentLists
+            requiredComponentList = (replicate (fromIntegral s) w)
+            extended_L = customizedComponentLists ++ requiredListsFilledWithValue
+            cropped_L = take (fromInteger z) customizedComponentLists
             customizedComponentLists = [cropOrExtendCols x | x <- l]
                 where 
                     cropOrExtendCols x
                         | length x < (fromIntegral s) = x ++ replicate ((fromIntegral s) - length x) w
                         | otherwise = take (fromIntegral s) x
-            requiredListsFilledWithWord = (replicate ((fromIntegral z) - (length l)) (replicate (fromIntegral s) w))
+            requiredListsFilledWithValue = (replicate ((fromIntegral z) - (length l)) requiredComponentList)
 
