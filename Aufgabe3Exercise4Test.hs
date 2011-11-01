55db4f954fa80ee45ba80b2844f406c67565dddd
Claus Polanka <claus.polanka@chello.at>
2011-11-01 14:05:27 +0100

    Passed all acceptance tests for assignment 3 exercise 4.

--- Aufgabe3Exercise4Test.hs
+++ Aufgabe3Exercise4Test.hs
@@ -9,6 +9,19 @@ funkProgAcceptanceTest1 =
 funkProgAcceptanceTest2 = 
     TestCase (assertEqual "Skalarprodukt" 32 (sp [[1, 2, 3]] [[4], [5], [6]] 3 1))
 
+funkProgAcceptanceTest3 = 
+    TestCase (assertEqual "Skalarprodukt" 10 (sp [[1, 2, 3]] [[4, 5, 6]] 4 1))
+
+
+funkProgAcceptanceTest4 = 
+    TestCase (assertEqual "Skalarprodukt" 33 (sp [[1, 2, 3]] [[4], [5], [6]] 4 1))
+
+funkProgAcceptanceTest5 = 
+    TestCase (assertEqual "Skalarprodukt" 34 (sp [[1, 2, 3], [6, 6, 7, 8], [3, 45]] [[4, 5, 6], [1, 2]] 4 4))
+
+funkProgAcceptanceTest6 = 
+    TestCase (assertEqual "Skalarprodukt" 35 (sp [] [[2, 3, 4]] 2 5))
+
 allTests = 
     TestList [
         TestLabel 
@@ -17,7 +30,23 @@ allTests =
 
         TestLabel 
         " FunkProg Acceptance Test 2." 
-        funkProgAcceptanceTest2
+        funkProgAcceptanceTest2,
+
+        TestLabel 
+        " FunkProg Acceptance Test 3." 
+        funkProgAcceptanceTest3,
+
+        TestLabel 
+        " FunkProg Acceptance Test 4." 
+        funkProgAcceptanceTest4,
+
+        TestLabel 
+        " FunkProg Acceptance Test 5." 
+        funkProgAcceptanceTest5,
+
+        TestLabel 
+        " FunkProg Acceptance Test 6." 
+        funkProgAcceptanceTest6
     ]
 
 main = do runTestTT allTests
