12aed35dc34b506e9ec2f6a779d5864ab596a5a9
Claus Polanka <claus.polanka@chello.at>
2011-11-12 13:12:37 +0100

    Implemented exercise 2 of assignment 2

--- Aufgabe4.hs
+++ Aufgabe4.hs
@@ -21,7 +21,18 @@ msk (comp_L, z, s, w) sk = [map (*sk) x | x <- (anp2 comp_L z s w)]
 
 -- Beispiel 2
 mm :: ProtoprotoMatrix -> ProtoprotoMatrix -> Typung_mnpw -> Matrix
-mm p1 p2 (z, sz, s, w) = []
+mm p1 p2 (z, sz, s, w) = calculateProductOf m1 m2
+    where
+        m1 = [x | x <- (anp2 p1 z sz w)]
+        m2 = anp2 p2 sz s w
+
+
+calculateProductOf m1 m2 = [foo2 rowVector m2 | m_i <- [0..(length m1 - 1)], let rowVector = (m1 !! m_i)]
+
+foo2 rowVector m2 = [productOf rowVector columnVector | p_i <- [0..(p - 1)], let columnVector = [x !! p_i | x <- m2]]
+    where 
+        productOf rV cV = sum (zipWith (*) rV cV)
+        p = length (m2 !! 0)
 
 -- Beispiel 3
 ms :: ProtoprotoMatrix -> ProtoprotoMatrix -> Typung_mnw -> Matrix
---
+++ Aufgabe4Exercise2Test.hs
@@ -0,0 +1,46 @@
+module Aufgabe4Exercise4Test where
+
+import HUnit
+import Aufgabe4
+
+productOfTwoEqualOneTimesOneMatrices = 
+    TestCase (assertEqual "Matrices-Product" [[1]] (mm [[1]] [[1]] (1, 1, 1, 2)))
+
+productOfTwoUnequalOneTimesOneMatrices = 
+    TestCase (assertEqual "Matrices-Product" [[2]] (mm [[2]] [[1]] (1, 1, 1, 2)))
+
+funkProgAcceptanceTest1 =
+    TestCase (assertEqual "Matrices-Product" [[22, 28], [49, 64]] (mm [[1, 2, 3], [4, 5, 6]] [[1, 2], [3, 4], [5, 6]] (2, 3, 2, 9)))
+
+funkProgAcceptanceTest2 =
+    TestCase (assertEqual "Matrices-Product" [[ 7, 10, 3, 3], 
+                                              [19, 28, 9, 9], 
+                                              [ 4,  6, 2, 2]] (mm [[1, 2, 3], [4, 5, 6]] [[1, 2], [3, 4], [5, 6]] (3, 2, 4, 1)))
+
+funkProgAcceptanceTest3 =
+    TestCase (assertEqual "Matrices-Product" [[22]] (mm [[1, 2, 3], [4, 5, 6]] [[1, 2], [3, 4], [5, 6]] (1, 3, 1, 9)))
+
+allTests = 
+    TestList [
+        TestLabel 
+        " Given two equal 1x1 matrices multiply them." 
+        productOfTwoEqualOneTimesOneMatrices,
+
+        TestLabel 
+        " Given two unequal 1x1 matrices multiply them." 
+        productOfTwoUnequalOneTimesOneMatrices,
+
+        TestLabel 
+        " FunkProg acceptance test 1." 
+        funkProgAcceptanceTest1,
+
+        TestLabel 
+        " FunkProg acceptance test 2." 
+        funkProgAcceptanceTest2,
+
+        TestLabel 
+        " FunkProg acceptance test 3." 
+        funkProgAcceptanceTest3
+    ]
+
+main = do runTestTT allTests
