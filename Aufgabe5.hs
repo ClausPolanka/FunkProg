18ea4c768c66788192677551acb5e92c822636d0
claus.polanka@chello.at <claus.polanka@chello.at>
2011-11-18 14:48:51 +0100

    Converted tabs to spaces.

--- Aufgabe5.hs
+++ Aufgabe5.hs
@@ -15,20 +15,20 @@ ctxwords = ["Test2 Test3  T", "T  Test1 Test2 Test3", "Test1  T Test2 Test3"]
 
 unixtac :: String -> String
 unixtac s = withoutLastNewLine text
-	where text = unlines . reverse . lines $ s
+    where text = unlines . reverse . lines $ s
 
 withoutLastNewLine :: String -> String
 withoutLastNewLine s = take (length s - 1) s 
 
 unixhead :: Int -> String -> String
 unixhead n text = withoutLastNewLine nLines
-	where nLines = unlines . take n  $ lines text
+    where nLines = unlines . take n  $ lines text
 
 unixtail :: Int -> String -> String
 unixtail n text = withoutLastNewLine nLines
-	where 
-		nLines = unlines . drop (length linesAsList - n) $ linesAsList
-		linesAsList = lines text
+    where 
+        nLines = unlines . drop (length linesAsList - n) $ linesAsList
+        linesAsList = lines text
 
 unixgrep :: String -> String -> String
 unixgrep s1 s2 = s1
