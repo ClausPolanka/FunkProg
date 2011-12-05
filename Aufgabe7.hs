d39dec56fd4e9f3c88b73b5e37aa3af43b5133e4
Claus Polanka <claus.polanka@chello.at>
2011-12-05 17:21:24 +0100

    Init of assignment 7.

---
+++ Aufgabe7.hs
@@ -0,0 +1,31 @@
+module Aufgabe7 where
+
+type Vertex = Integer
+type Origin = Vertex
+type Destination = Vertex
+type Key = Integer
+type Name = Integer
+
+data BTree a = BLeaf Key a | BNode Key a (BTree a) (BTree a) deriving Show
+data LTree a = LNode Key a [(LTree a)] deriving Show
+data ALgraph = ALg [(Origin, [Destination])] deriving (Eq,Show)
+
+class Structure s where
+	noOfSources :: s -> Integer
+	noOfSinks :: s -> Integer
+	notSourceConnected :: s -> [Name]
+	notSinkConnected :: s -> [Name]
+
+-- Teil 2
+
+type State = Integer
+type StartState = State
+type AcceptingStates = [State]
+type Word a = [a]
+type Row a = [[a]]
+type Automaton a = AMgraph a
+
+data AMgraph a = AMg [(Row a)] deriving (Eq,Show)
+
+accept :: Eq a => (Automaton a) -> StartState -> AcceptingStates -> (Word a) -> Bool
+accept _ _ _ _ = True
